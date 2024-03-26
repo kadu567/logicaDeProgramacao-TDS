5d56f46c5d5814648835521fbc0e77b3523c44fb
kadu567 <162385570+kadu567@users.noreply.github.com>
2024-03-26 00:49:26 +0000

    main.cpp

--- exerciciolaranja1.cpp
+++ exerciciolaranja1.cpp
@@ -7,10 +7,21 @@ int main(){
     
     bool resultado = (x < 5 && 5 < z);
 
-    printf("Resultado: %d /n", resultado);
+    printf("Resultado: %d \n", resultado);
 
     
+
+
+    int a = 7;
+    int b = 10;
+    int c = 6;
+    int d = 8;
+
+    printf("soma: %d + %d = %d\n", num1, num2, soma);
     
+    bool res = ((7 == 10) || 6< 8 );
+
+    printf("Resultado: %d \n", res); 
     
     return 0;
 }
